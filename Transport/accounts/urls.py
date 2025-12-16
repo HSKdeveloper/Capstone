4007d1eca4bcadd9c08f70c912a241589ce7110b
norah alminhali <norahmu515@gmail.com>
2025-12-16 21:17:54 +0300

    Page accounts

--- Transport/accounts/urls.py
+++ Transport/accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-
 from . import views
-
 
 app_name = "accounts"
 
-
 urlpatterns=[
+    path('signup/', views.sign_up, name='sign_up'),
+    path('signin/', views.sign_in, name='sign_in'),
+    path('logout/', views.log_out, name='log_out'),
+    path('profile/', views.profile_view, name='profile_view'),
+    path('update_profile/', views.update_profile, name='update_profile'),
 ]
--- Transport/accounts/views.py
+++ Transport/accounts/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.http import HttpRequest, HttpResponse
+from .models import Profile
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def sign_up(request: HttpRequest):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists.')
+            return redirect('accounts:sign_up')
+        
+        user = User.objects.create_user(username=username, password=password, email=email)
+        Profile.objects.create(user=user)
+        login(request, user)
+        return redirect('main:home_view')
+    
+    return render(request, 'accounts/signup.html')
+
+def sign_in(request: HttpRequest):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('accounts:profile_view')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('accounts:sign_in')
+    
+    return render(request, 'accounts/signin.html')
+
+def log_out(request: HttpRequest):
+
+    logout(request)
+    return redirect('accounts:sign_in')
+
+def profile_view(request: HttpRequest):
+    
+    return render(request, 'accounts/profile.html')
+
+def update_profile(request: HttpRequest):
+   
+    return render(request, 'accounts/update_profile.html')
